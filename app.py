6c537440d27d9a4f072c45783ed524797b1ef477
Priyam Mitra <mpriyam@thoughtworks.com>
2018-08-26 00:17:41 +0530

    pretty print matrix

--- app.py
+++ app.py
@@ -9,7 +9,23 @@ def readConfig():
             print(exc)
     return fileContent
 
+def printLeftAlign(item):
+    print("%-25s" % item, end="")
+
+def prettyPrintMatrix(matrix):
+    columnCount=len(matrix[0])
+    printLeftAlign("")
+    for columnNumber in range(1, columnCount+1):
+        printLeftAlign("Slot "+str(columnNumber))
+    print()
+    for rowIndex, row in enumerate(matrix):
+        printLeftAlign("Round "+str(rowIndex+1))
+        for column in row:
+            printLeftAlign(""+str(column))
+        print()
+
 config=readConfig()
-matrix=SpeedbackMatrix()
-print(matrix.getFinalGrid(config['members']))
+matrix=SpeedbackMatrix().getFinalGrid(config['members'])
+
+prettyPrintMatrix(matrix)
 
