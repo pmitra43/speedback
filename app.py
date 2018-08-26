6357e998ab75450eb6b5f16c7ae78e5a793a831e
Priyam Mitra <mpriyam@thoughtworks.com>
2018-08-26 09:45:58 +0530

    created RoundTimer in timer to handle all the timer and countdown and printing

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import yaml
 import sys
-import time
-import subprocess
 from speedback import SpeedbackMatrix
+from timer import RoundTimer
 
 def readConfig():
     with open("config.yml", 'r') as stream:
@@ -16,9 +15,6 @@ def printLeftAlign(item):
     sys.stdout.flush()
     sys.stdout.write("%-25s" % item)
 
-def say(item):
-    subprocess.call('say -r 200 '+str(item), shell=True)
-
 def prettyPrintMatrix(matrix):
     columnCount=len(matrix[0])
     printLeftAlign("")
@@ -31,38 +27,12 @@ def prettyPrintMatrix(matrix):
             printLeftAlign(""+str(column))
         print()
 
-def doCountdown(roundCount):
-    say("next round starting in")
-    for secondsLeft in range(3,0,-1):
-        printLeftAlign("\rRound %d coming up in %02d seconds" % (roundCount, secondsLeft%60))
-        say(secondsLeft)
-        time.sleep(0.6)
-
-def showTimer(upperLimit, lowerLimit):
-    for secondsLeft in range(int(upperLimit),int(lowerLimit), -1):
-        printLeftAlign("\rOngoing Round %d : %02d:%02d minutes left" % (roundCount, int(secondsLeft/60), int(secondsLeft%60)))
-        time.sleep(1)
-
-def startFeedbackRoundTimer(roundCount, pairFeedbackTimeInMinutes):
-    doCountdown(roundCount)
-    showTimer(pairFeedbackTimeInMinutes*60, pairFeedbackTimeInMinutes*30)
-    say("half time. It's your pair's turn now")
-    showTimer(pairFeedbackTimeInMinutes*30, 0)
-    printLeftAlign("\rRound %d finished. Time to switch pair\n" % (roundCount))
-    say("time up")
-
-def startSwitchTimer(roundCount, pairSwitchTimeInSeconds):
-    for secondsLeft in range(int(pairSwitchTimeInSeconds),3, -1):
-        printLeftAlign("\rRound %d coming up in %d seconds" % (roundCount, secondsLeft))
-        time.sleep(1)
-
 config=readConfig()
 members=config['members']
 duration=config['duration']
 matrix=SpeedbackMatrix().getFinalGrid(members)
+timer=RoundTimer()
 
 prettyPrintMatrix(matrix)
 print()
-for roundCount in range(1, len(members)):
-    startSwitchTimer(roundCount, duration['pairSwitchTimeInSeconds'])
-    startFeedbackRoundTimer(roundCount, duration['pairFeedbackTimeInMinutes'])
+timer.startRounds(duration, len(members))
