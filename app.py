b0c0481344262e824600b754fd0f87727113b1c3
Priyam Mitra <mpriyam@thoughtworks.com>
2018-08-26 10:25:00 +0530

    created ConsoleIO class to get user inputs from console

--- app.py
+++ app.py
@@ -3,6 +3,56 @@ import sys
 from speedback import SpeedbackMatrix
 from timer import RoundTimer
 
+class ConsoleIO:
+    def printLeftAlign(self, item):
+        sys.stdout.flush()
+        sys.stdout.write("%-25s" % item)
+
+    def prettyPrintMatrix(self, matrix):
+        columnCount=len(matrix[0])
+        self.printLeftAlign("")
+        for columnNumber in range(1, columnCount+1):
+            self.printLeftAlign("Slot "+str(columnNumber))
+        print()
+        for rowIndex, row in enumerate(matrix):
+            self.printLeftAlign("Round "+str(rowIndex+1))
+            for column in row:
+                self.printLeftAlign(""+str(column))
+            print()
+
+    def calculateTotalDuration(self, feedbackTime, switchTime, memberLength):
+        timeInSec = feedbackTime*60*(memberLength-1)
+        timeInSec = timeInSec+(switchTime+1)*(memberLength-1)
+        return "%02d:%02d" % (int(timeInSec/60), int(timeInSec%60))
+
+    def validatedWithUser(self, duration, memberLength):
+        feedbackTime=duration['pairFeedbackTimeInMinutes']
+        switchTime=duration['pairSwitchTimeInSeconds']
+        while(1):
+            timeRequired = self.calculateTotalDuration(feedbackTime, switchTime, memberLength)
+            print("With %d minutes per round and %d seconds to switch, you will need %s minutes" % 
+                (feedbackTime, switchTime, timeRequired))
+            choice=input("Do you want to start/edit/quit? (s-start/e-edit/q-quit): ")
+            if(choice=='s'):
+                duration['pairFeedbackTimeInMinutes']=feedbackTime
+                duration['pairSwitchTimeInSeconds']=switchTime
+                return True
+            elif(choice=='e'):
+                newFeedbackTime = input("Provide new pair feedback time in minutes :(%d):" % (feedbackTime))
+                newSwitchTime = input("Provide new pair switch in seconds :(%d):" % (switchTime))
+                try:
+                    if(newFeedbackTime!=""):
+                        feedbackTime=int(newFeedbackTime)
+                    if(newSwitchTime!=""):
+                        switchTime=int(newSwitchTime)
+                except:
+                    print("Invalid option")
+                    continue
+            elif(choice=='q'):
+                return False
+            else:
+                print("Wrong choice")
+
 def readConfig():
     with open("config.yml", 'r') as stream:
         try:
@@ -11,28 +61,14 @@ def readConfig():
             print(exc)
     return fileContent
 
-def printLeftAlign(item):
-    sys.stdout.flush()
-    sys.stdout.write("%-25s" % item)
-
-def prettyPrintMatrix(matrix):
-    columnCount=len(matrix[0])
-    printLeftAlign("")
-    for columnNumber in range(1, columnCount+1):
-        printLeftAlign("Slot "+str(columnNumber))
-    print()
-    for rowIndex, row in enumerate(matrix):
-        printLeftAlign("Round "+str(rowIndex+1))
-        for column in row:
-            printLeftAlign(""+str(column))
-        print()
-
 config=readConfig()
 members=config['members']
 duration=config['duration']
-matrix=SpeedbackMatrix().populateGrid(members)
-timer=RoundTimer()
+consoleIO=ConsoleIO()
 
-prettyPrintMatrix(matrix)
-print()
-timer.startRounds(duration, len(members))
+if(consoleIO.validatedWithUser(duration, len(members))):
+    matrix=SpeedbackMatrix().populateGrid(members)
+    timer=RoundTimer()
+    consoleIO.prettyPrintMatrix(matrix)
+    print(duration)
+    timer.startRounds(duration, len(members))
