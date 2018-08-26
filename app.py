9c73d9618042a96539d1ab58fb701c0af41ed630
Priyam Mitra <pmitra43@gmail.com>
2018-08-26 23:48:11 +0530

    created App class

--- app.py
+++ app.py
@@ -4,23 +4,30 @@ import yaml
 from speedback import SpeedbackMatrix
 from util import RoundTimer, ConsoleIO
 
-def readConfigFile():
-    with open("config.yml", 'r') as stream:
-        try:
-            fileContent=yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-    return fileContent
+class App:
+    def __init__(self):
+        self.consoleIO=ConsoleIO()
+        self.timer=RoundTimer()
+        self.speedbackMatrix=SpeedbackMatrix()
 
-config=readConfigFile()
-members=config['members']
-duration=config['duration']
-consoleIO=ConsoleIO()
+    def readConfigFile(self):
+        with open("config.yml", 'r') as stream:
+            try:
+                fileContent=yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return fileContent
 
-if(consoleIO.validatedWithUser(duration, len(members))):
-    matrix=SpeedbackMatrix().populateGrid(members)
-    timer=RoundTimer()
-    consoleIO.prettyPrintMatrix(matrix)
-    print(duration)
-    timer.startRounds(duration, len(members))
-print("Thank you")
+    def runApplication(self):
+        config=self.readConfigFile()
+        members=config['members']
+        duration=config['duration']
+
+        if(self.consoleIO.validatedWithUser(duration, len(members))):
+            matrix=self.speedbackMatrix.populateGrid(members)
+            self.consoleIO.prettyPrintMatrix(matrix)
+            print(duration)
+            self.timer.startRounds(duration, len(members))
+        print("Thank you")
+
+App().runApplication()
